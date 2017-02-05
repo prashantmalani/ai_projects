b7c9df31b0a63a80277dfbe73f4269835f01c6ff
Prashant Malani <p.malani@gmail.com>
2017-02-04 20:48:15 -0800

    P1: Added some error checking

--- project1/driver.py
+++ project1/driver.py
@@ -6,12 +6,26 @@ from astar_solver import astar_solver
 from ida_solver import ida_solver
 import Queue
 import sys
+import math
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print "Invalid number of arguments"
+        sys.exit(0)
     algo = sys.argv[1]
     init_str = sys.argv[2]
     init_list = [int(x) for x in init_str.split(',')]
+
+    if (math.sqrt(len(init_list)) ** 2) != len(init_list):
+        print "Not a square list"
+        sys.exit(0)
+
+    # Check whether all the numbers are there in the board
+    for i in range(len(init_list)):
+        if i not in init_list:
+            print "We found a missing number in the list"
+            sys.exit(0)
     init_vals = board(init_list)
     # Create the root node
     start_node = node(None, init_vals, 0, None)
