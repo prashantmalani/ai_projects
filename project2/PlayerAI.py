9582fd07682e41025e745677a48a31fc0408fbf7
Prashant Malani <p.malani@gmail.com>
2017-03-10 02:49:43 -0800

    Add a couple of heuristics
    1. Max valued tile at corners (weight 50)
    2. Percentage of free tiles (weight 50)
    3. Percentage of rows that are non monotonic (weight -30)

--- project2/PlayerAI.py
+++ project2/PlayerAI.py
@@ -6,7 +6,7 @@ import time
 
 class PlayerAI(BaseAI):
 
-    MAX_DEPTH = 4
+    MAX_DEPTH = 5
 
     def getMove(self, grid):
         #moves = grid.getAvailableMoves()
@@ -20,10 +20,86 @@ class PlayerAI(BaseAI):
         return child
 
     def eval_func(self, grid):
-        """ Initially, let's keep the eval function as purely the max value
-        tile. We can iterate later.
+        """ Evaluation function of the current grid.
         """
-        return grid.getMaxTile()
+        # Each heuristic is scored on 100, and we give each one weights.
+        w1 = 50 # 50 percent weightage to max tile in corner
+        w2 = 50 # 50 percent weightage to number of free spaces
+        w3 = -30 # penalty weightage if there are any non-monotonic rows/columns
+
+        max_val = grid.getMaxTile()
+        #  print "Max tile value is:" + str(max_val)
+
+        # h1 : Position of max tile: Corners scored highest, mid scored lowest
+        locs = [(x,y) for (x,y) in itertools.product(range(4), range(4)) if grid.map[x][y] == max_val]
+        # Pic any max val tile, in case there are many
+
+        one_max_loc = locs[0]
+        score = 4
+        if (one_max_loc[0] % (grid.size - 1) > 0) :
+            score /= 2
+        if (one_max_loc[1] % (grid.size - 1) > 0) :
+            score /= 2
+        h1 = (score * 25 )# normalize to 100 ; h1 = (score * 100) / 4
+
+        # h2 : Number of free tiles: Normalize against max free Tiles
+        num_free_tiles = len(grid.getAvailableCells())
+        h2 = (num_free_tiles * 100) / 16
+
+        # calculate number of non-monotonic rows / columns out of 8.
+        # First check the rows
+        non_monot = 0;
+        for i in range(4):
+            old_diff = 0
+            new_diff = 0
+            for j in range(1,4):
+                new_diff = grid.map[i][j] - grid.map[i][j-1]
+                # If there is no change, continue looking at the next value
+                if new_diff == 0:
+                    continue
+
+                if new_diff > 0:
+                    new_diff = 1
+                else:
+                    new_diff = -1
+
+                # The logic here is as follows
+                # old_diff tracks the current trend; if it is +ve, we've seen at
+                # least one increase in value. if it is -ve, we've seen at least
+                # one decrease.
+                # so as long as old_diff * new_diff is >= 0 , this means there
+                # hasn't been any sign inversion.
+                if (new_diff * old_diff) < 0:
+                    non_monot += 1
+                    break
+                if new_diff != 0 and old_diff == 0:
+                    old_diff = new_diff
+
+        for i in range(4):
+            old_diff = 0
+            new_diff = 0
+            for j in range(1,4):
+                new_diff = grid.map[j][i] - grid.map[j-1][i]
+                # If there is no change, continue looking at the next value
+                if new_diff == 0:
+                    continue
+                if new_diff > 0:
+                    new_diff = 1
+                else:
+                    new_diff = -1
+
+                if (new_diff * old_diff) < 0:
+                    non_monot += 1
+                    break
+                if new_diff != 0 and old_diff == 0:
+                    old_diff = new_diff
+
+        h3 = (non_monot * 100) / 8
+
+        score = (h1*w1 + h2*w2 + h3*w3) / (w1 + w2 + w3)
+        return score
+
+
 
     def maximize(self, grid, alpha, beta, depth):
         # Terminal state
@@ -45,7 +121,8 @@ class PlayerAI(BaseAI):
                 max_utility = utility
             if max_utility >= beta:
                 break
-            if max_utility >= alpha:
+            if max_utility > alpha:
+                #print ('ALPHA updated:%f' % max_utility)
                 alpha = max_utility
 
         return max_child, max_utility
@@ -56,7 +133,12 @@ class PlayerAI(BaseAI):
         After implementing certain heuristics to make the opponent
         adversarial.
         """
-        return grid.getAvailableCells()
+        orig_list = grid.getAvailableCells()
+        loc_list = [(x,y) for x,y in orig_list if (x is 0 or x is 3) and (y is 0 or y is 3)]
+        if len(loc_list) is 0:
+            return orig_list
+        return loc_list
+
 
     def minimize(self, grid, alpha, beta, depth):
         # Terminal state
@@ -75,15 +157,16 @@ class PlayerAI(BaseAI):
         for cur_location,cur_val in move_val_pairs:
             x = grid.clone()
             x.insertTile(cur_location, cur_val)
+            # print "Tile inserted:" + str(cur_val) + ", location:" + str(cur_location)
             _, utility = self.maximize(x, alpha, beta, depth + 1)
 
             if utility < min_utility:
                 min_child = x
                 min_utility = utility
-
             if min_utility <= alpha:
                 break
-            if min_utility <= beta:
+            if min_utility < beta:
+                #print ('BETA updated:%f' % min_utility)
                 beta = min_utility
-
+        #print('MIN: depth=%d, min_util=%f' % (depth, min_utility))
         return min_child, min_utility
